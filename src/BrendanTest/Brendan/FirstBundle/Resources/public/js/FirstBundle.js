e017abdfadb35cbaa4af2aa9b11ee6d2f67db425
Brendan McGarrett <bmcgarrett@gmail.com>
2013-07-09 14:48:50 -0400

    Updated to include Bundle.js with jQuery ready function

---
+++ src/BrendanTest/Brendan/FirstBundle/Resources/public/js/FirstBundle.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: bmcgarr
+ * Date: 7/9/13
+ * Time: 2:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function(){
+
+});
